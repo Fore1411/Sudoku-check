e22bfbaa7c850800f6f297e6a88622c26689121d
Laura <fore1411@Lauras-MacBook-Pro-2.local>
2024-06-18 19:17:38 -0400

    Initial instructions added on boardy. py

--- board.py
+++ board.py
@@ -1,3 +1,5 @@
+#With board.py create a board totally solved and take the result to replace board on final.py to validate the board.
+
 from sudoku import Sudoku 
 import random   
 
@@ -5,14 +7,5 @@ puzzle = Sudoku(3).difficulty(0.5)
 puzzle.show()
 solution=puzzle.solve()
 solution.show()
-# #loop over the solution boards[0]
-# for i in range (9):
 print(solution.board)
-    # right_numbers=[1, 2, 3, 4, 5, 6, 7, 8, 9]
-    # if solution.board == right_numbers:
-    #     print("correct")
-    # else:
-    #     print("Try Again")
     
-    # With solution board create a def called repeated_numbers that just print the numbers that are not supossed to be in each list. 
-    #print(Sudoku.index)
